c4af8d68b7b52024d4b1182d7484c500b0e28279
nivak22 <90485437+nivak22@users.noreply.github.com>
2025-01-09 20:00:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const app = express(); // Definición de app
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); // Uso de app después de su definición
 const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
